05e2533e1d2b7d73a8744f4b31462b2a22af7331
rvdheij <rvdheij@gmail.com>
2015-03-03 00:23:14 +0100

    Layout and pretty formatting

--- workload/poissinv.c
+++ workload/poissinv.c
@@ -18,27 +18,27 @@
 // The poissinv function uses an iterative process to produce the
 // poisson distribution, which fails when the lambda value is too
 // high and e**(-lambda) becomes too small to work with.
-// For a large average, we scale down to 256 and use a linear 
+// For very large averages, we scale down to 256 and use a linear
 // random function to interpolate.
 
-int poissinv(int lambda) {
-  int i;
-  double p = 1;
-  double scale = 1;
+int poissinv(int lambda)
+{
+        int i;
+        double p = 1;
+        double scale = 1;
 
-  if (lambda>255) {
-    scale = (double) lambda/256;
-    lambda = 256;
-  }
+        if (lambda>255) {
+                scale = (double) lambda/256;
+                lambda = 256;
+        }
 //          	                            Compute e**(-n)
-  for (i=1; i <= lambda; i++) p = p / 2.71828182845904523536l;
+        for (i=1; i <= lambda; i++) p = p / 2.71828182845904523536l;
 
-  double f = 0;
-  double r = drand48();
-  for (i=0; (r>f) && i++<=(lambda*5); ) {
-    p = p * ((double) lambda / (double) i);
-    f = f + p;
-  }
-  return (i + drand48()) * scale;
+        double f = 0;
+        double r = drand48();
+        for (i=0; (r>f) && i++<=(lambda*5); ) {
+                p = p * ((double) lambda / (double) i);
+                f = f + p;
+        }
+        return (i + drand48()) * scale;
 }
-
